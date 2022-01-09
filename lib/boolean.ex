6194a9d5f0bd641642d3c7a4af4857579ae16464
febil va <febil.tvr@gmail.com>
2022-01-09 22:31:15 +0530

    add int and float parser

--- lib/boolean.ex
+++ lib/boolean.ex
@@ -1,18 +1,39 @@
 defmodule Boolean do
-  @moduledoc """
-  Documentation for `Boolean`.
-  """
+  import NimbleParsec
 
-  @doc """
-  Hello world.
+  digits = [?0..?9] |> ascii_string(min: 1) |> label("digits")
 
-  ## Examples
+  float =
+    optional(string("-"))
+    |> concat(digits)
+    |> ascii_string([?.], 1)
+    |> concat(digits)
+    |> reduce(:to_float)
+    |> label("float")
 
-      iex> Boolean.hello()
-      :world
+  int =
+    optional(string("-"))
+    |> concat(digits)
+    |> reduce(:to_integer)
+    |> label("integer")
 
-  """
-  def hello do
-    :world
+  defp to_float(acc), do: acc |> Enum.join() |> String.to_float()
+  defp to_integer(acc), do: acc |> Enum.join() |> String.to_integer(10)
+
+  var = ascii_char([?a..?z]) |> repeat(ascii_char([?a..?z])) |> label("var")
+  num = choice([float, int]) |> label("number")
+
+  defparsec(
+    :parse_string,
+    choice([var, num])
+  )
+
+  def parse(string) do
+    parse_string(string)
+    |> unwrap
   end
+
+  defp unwrap({:ok, [acc], "", _, _, _}), do: acc
+  defp unwrap({:ok, _, rest, _, _, _}), do: {:error, "could not parse", rest}
+  defp unwrap({:error, reason, rest, _, _, _}), do: {:error, reason, rest}
 end
--- test/boolean_test.exs
+++ test/boolean_test.exs
@@ -4,5 +4,18 @@ defmodule BooleanTest do
 
   test "parse an integer" do
     assert Boolean.parse("1") == 1
+    assert Boolean.parse("-1") == -1
+  end
+
+  @err "expected digits while processing integer inside number"
+  test "parse a float number" do
+    assert Boolean.parse("1.0") == 1.0
+    assert Boolean.parse("-1.0") === -1.0
+    assert Boolean.parse("0.0001") === 0.0001
+    assert {:error, @err, ".0001"} = Boolean.parse(".0001")
+  end
+
+  test "parse a variable" do
+    assert Boolean.parse("a") == {:var, "a"}
   end
 end
