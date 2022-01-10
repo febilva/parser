65a3e359534e0fb01f84bcb41cd7b3c59a3e630b
Febil <febil.tvr@gmail.com>
2022-01-10 16:09:53 +0530

    add var, string

--- lib/boolean.ex
+++ lib/boolean.ex
@@ -1,6 +1,13 @@
 defmodule Boolean do
   import NimbleParsec
 
+  @reserved_sym ["true", "false"]
+
+  true_ = string("true") |> replace(true)
+  false_ = string("false") |> replace(false)
+
+  boolean = [true_, false_] |> choice |> label("boolean")
+
   digits = [?0..?9] |> ascii_string(min: 1) |> label("digits")
 
   float =
@@ -11,21 +18,51 @@ defmodule Boolean do
     |> reduce(:to_float)
     |> label("float")
 
+  defp to_float(acc), do: acc |> Enum.join() |> String.to_float()
+
   int =
     optional(string("-"))
     |> concat(digits)
     |> reduce(:to_integer)
     |> label("integer")
 
-  defp to_float(acc), do: acc |> Enum.join() |> String.to_float()
   defp to_integer(acc), do: acc |> Enum.join() |> String.to_integer(10)
 
-  var = ascii_char([?a..?z]) |> repeat(ascii_char([?a..?z])) |> label("var")
   num = choice([float, int]) |> label("number")
+
+  var =
+    ascii_char([?a..?z])
+    |> repeat(ascii_char([?a..?z, ?A..?Z, ?0..?9, ?_]))
+    |> post_traverse(:to_varname)
+    |> unwrap_and_tag(:var)
+    |> label("var")
+
+  defp to_varname(_rest, acc, context, _line, _offset) do
+    name = acc |> Enum.reverse() |> List.to_string()
+
+    if name in @reserved_sym do
+      {:error, name <> " is a reserved symbol"}
+    else
+      {[name], context}
+    end
+  end
+
+  string =
+    ignore(ascii_char([?"]))
+    |> repeat_while(
+      utf8_char([]),
+      {:not_quote, []}
+    )
+    |> ignore(ascii_char([?"]))
+    |> reduce({List, :to_string, []})
+    |> label("string")
+
+  defp not_quote(<<?", _::binary>>, context, _, _), do: {:halt, context}
+  defp not_quote(_, context, _, _), do: {:cont, context}
 
   defparsec(
     :parse_string,
-    choice([var, num])
+    [boolean, num, string, var] |> choice
   )
 
   def parse(string) do
--- test/boolean_test.exs
+++ test/boolean_test.exs
@@ -2,12 +2,17 @@ defmodule BooleanTest do
   use ExUnit.Case
   # doctest Boolean
 
+  test "parse a boolean" do
+    assert Boolean.parse("true") == true
+    assert Boolean.parse("false") == false
+  end
+
   test "parse an integer" do
     assert Boolean.parse("1") == 1
     assert Boolean.parse("-1") == -1
   end
 
-  @err "expected digits while processing integer inside number"
+  @err "expected var while processing boolean or number or string or var"
   test "parse a float number" do
     assert Boolean.parse("1.0") == 1.0
     assert Boolean.parse("-1.0") === -1.0
@@ -17,5 +22,11 @@ defmodule BooleanTest do
 
   test "parse a variable" do
     assert Boolean.parse("a") == {:var, "a"}
+    assert Boolean.parse("aAAA9_") == {:var, "aAAA9_"}
+    assert Boolean.parse("aAAA9____") == {:var, "aAAA9____"}
+    assert Boolean.parse("a-9") == {:error, "could not parse", "-9"}
+    assert Boolean.parse("A") == {:error, @err, "A"}
+    assert Boolean.parse("_A") == {:error, @err, "_A"}
+    assert Boolean.parse("A_") == {:error, @err, "A_"}
   end
 end
